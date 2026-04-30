14e7ea116c732eb1aa3a9d7a270ef7485a6361ea
sana-sagegami <sana01237@icloud.com>
2026-04-30 15:37:47 +0900

    .env.developmentを追加し、.gitignoreに.env.developmentを除外。vite.config.tsに/workerのプロキシ設定を追加

--- alive-frontend/vite.config.ts
+++ alive-frontend/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/worker': {
+        target: 'https://alive-worker.alive-sana.workers.dev',
+        changeOrigin: true,
+        rewrite: () => '/',
+      },
+    },
+  },
 })
